cc5e19a8bd20cbb689650a96db54ca5f783468b2
Your Name <you@example.com>
2022-10-06 11:18:52 +0100

    Reinicio

--- Astronauta/Assets/Scripts/PlayerController.cs
+++ Astronauta/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,8 +11,18 @@ public class PlayerController : MonoBehaviour
     Vector2 direccion;
 
     [SerializeField]
-
     float impulse = 10f;
+
+    [SerializeField]
+    TextMeshProUGUI labelFuel;
+
+    [SerializeField]
+    float fuel = 100f;
+
+    bool isPlaying = false;
+
+    [SerializeField]
+    GameObject prefabParticles;
 
     void Start()
     {
@@ -24,14 +35,50 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direccion.x = Input.GetAxis("Horizontal") * Time.deltaTime * impulse;
+        labelFuel.text = fuel.ToString("0") + "%";
 
-        direccion.y = Input.GetAxis("Vertical") * Time.deltaTime * impulse;
+        if (fuel > 0)
+        {
+            isPlaying = true;
+
+            if (isPlaying == true)
+            {
+                direccion.x = Input.GetAxis("Horizontal") * Time.deltaTime * impulse;
+
+                direccion.y = Input.GetAxis("Vertical") * Time.deltaTime * impulse;
+
+                fuel -= Time.deltaTime;
+            }
+            
+        }
+
+        else
+        {
+            isPlaying = false;
+            fuel = 0f;
+        }
     }
 
     private void FixedUpdate()
     {
         body.AddForce(direccion, ForceMode2D.Impulse);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Gasolina")
+        {
+            fuel += 10f;
+            if (fuel > 100f)
+            {
+                fuel = 100f;
+            }
+        }
+
+        Instantiate(prefabParticles, collision.transform.position, collision.transform.rotation);
+
+        Destroy(collision.gameObject);
 
     }
 }
